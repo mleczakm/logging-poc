64e16158c4ef047d39c9c5f47ad7fa5aab6f68aa
Michał Mleczko <michal.mleczko@miinto.com>
2022-06-24 07:09:07 +0200

    Add filtering out report to interface

--- src/UserInterface/Http/GetReportAction.php
+++ src/UserInterface/Http/GetReportAction.php
@@ -10,13 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class GetReportAction
 {
-    public function __invoke(Request $request, Report $report): Response {
+    public function __invoke(Request $request, Report $report): Response
+    {
         $orderBy = $request->get('orderBy', 'id');
 
-        if (!in_array($orderBy, ['id', 'name', 'surname', 'payout'])) {
+        $supportedColumns = ['id', 'name', 'surname', 'payout', 'department'];
+
+        if (! in_array($orderBy, ['id', 'name', 'surname', 'payout', 'department'])) {
             return new Response(status: Response::HTTP_BAD_REQUEST);
         }
 
-        return new Response(content: $report->monthly($orderBy));
+        $filterOut = $request->get('filterOut', []);
+
+        if (! is_array($filterOut) || array_diff(array_keys($filterOut), $supportedColumns) !== []) {
+            return new Response(status: Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(content: $report->monthly($orderBy, $filterOut));
     }
 }
--- tests/Feature/GetReportActionTest.php
+++ tests/Feature/GetReportActionTest.php
@@ -40,6 +40,53 @@ final class GetReportActionTest extends WebTestCase
     }
 
     /** @test */
+    public function shouldFilterOutSupportedFields(): void
+    {
+        $client = self::createClient();
+        $client->request('POST',
+                         '/department',
+                         [
+                             'id' => $departmentId = Ulid::generate(),
+                             'name' => 'HR',
+                             'strategy' => [
+                                 'name' => 'constant',
+                                 'amount' => 100,
+                             ],
+                         ]);
+        $client->request('POST',
+                         '/user',
+                         [
+                             'id' => Ulid::generate(),
+                             'department_id' => $departmentId,
+                             'name' => 'Zichał',
+                             'surname' => 'Mleczko',
+                             'payout' => 123,
+                             'seniority' => 15
+                         ]);
+
+        $client->request('POST',
+                         '/user',
+                         [
+                             'id' => Ulid::generate(),
+                             'department_id' => $departmentId,
+                             'name' => 'Michał',
+                             'surname' => 'Mleczko',
+                             'payout' => 123,
+                             'seniority' => 15
+                         ]);
+
+        $client->request('GET', '/report',);
+
+        $allData = $client->getResponse()->getContent();
+        $client->request('GET', '/report?filterOut[name]=Zichał',);
+        $filtered = $client->getResponse()->getContent();
+
+        self::assertNotSame($allData, $filtered, $allData);
+        self::assertStringContainsString('1 rows', $filtered);
+        self::assertStringContainsString('2 rows', $allData);
+    }
+
+    /** @test */
     public function returnBadRequestOnNotSupportedColumn(): void
     {
         $client = self::createClient();
